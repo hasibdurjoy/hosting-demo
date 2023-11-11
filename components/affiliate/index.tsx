c1d84ac97de2c50d64b09035f0e0ae88e7c73257
hasibdurjoy <hasibdurjoy5@gmail.com>
2023-11-11 20:41:11 +0600

    remove

--- components/affiliate/index.tsx
+++ components/affiliate/index.tsx
@@ -2,7 +2,6 @@ import ChoseAreaHomeOne from "../homes/home/ChoseAreaHomeOne";
 import PriceArea from "../hosting/PriceArea";
 import FooterOne from "../layout/footers/FooterOne";
 import HeaderOne from "../layout/headers/header";
-import HeaderOne from "../layout/headers/header";
 import Breadcrumb from "./../common/breadcrumbs/breadcrumb";
 import FAQArea from "./FAQArea";
 import IntroVideoArea from "./IntroVideoArea";
